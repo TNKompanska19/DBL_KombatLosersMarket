101246e6c0fecd1fb917523511f1c96c5671079b
Pavel Kotorov <p.y.kotorov@student.tue.nl>
2025-06-18 21:35:02 +0200

    [DELTE] add_code.py placeholders

--- DBL_Data_Challenge/map/add_code.py
+++
@@ -1,3 +0,0 @@
-# add the code for the interactive map!!!
-
-print("Hello World!")
--- DBL_Data_Challenge/sentiment_analysis/add_code.py
+++
@@ -1,3 +0,0 @@
-# Add everything for sentiment analysis plots that are in the poster and some that are not in it, pls!!
-
-print("please...")
